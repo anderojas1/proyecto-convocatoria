2428af64349a3fe734a4ec7630d2289c0fb061ed
jhonkrave <jhonkrave@gmail.com>
2014-11-10 20:17:35 -0500

    conocimientos especificos

---
+++ src/Gui/VentanaConociemientosEspec.java
@@ -0,0 +1,241 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Gui;
+
+import controlador.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+/**
+ *
+ * @author jhonkrave
+ */
+public class VentanaConociemientosEspec extends JFrame {
+    
+    //container
+    private Container container;
+    
+    
+    
+    //paneles
+    private JPanel JPtitulo;
+    private JPanel JPconocimientos;
+    private JPanel JPbotones;
+    
+    //etiquetas
+    private JLabel JLtitulo;
+    private JLabel JLsubtitulo;
+    private JLabel JLsubtitulonivel;
+    private JLabel JLherram_Ofimatic;
+    private JLabel JLherram_Web2;
+    private JLabel JLherram_edic_multi;
+    private JLabel JLexp_des_cont_edu_dig;
+    private JLabel JLexp_des_lib_dig;
+    private JLabel JLexp_proc_elearning;
+    private JLabel JLexp_gest_proy_TIC;
+    private JLabel JLexp_des_elemnts_eval_comp;
+    
+    //combox
+    
+    /*
+    private JComboBox JCBherram_Ofimatic;
+    private JComboBox JCBherram_Web2;
+    private JComboBox JCBherram_edic_multi;
+    private JComboBox JCBexp_des_cont_edu_dig;
+    private JComboBox JCBexp_des_lib_dig;
+    private JComboBox JCBexp_proc_elearning;
+    private JComboBox JCBexp_gest_proy_TIC;
+    private JComboBox JCBexp_des_elemnts_eval_comp;
+    */
+    
+    private JComboBox [] niveles;
+  private String [] items = {"Regular","Bueno", "Muy Bueno"};
+    
+    //botnoes
+    
+    private JButton JBcanelar, JBback,JBnext;
+    
+    //otros
+    
+    private EventManager eventmanager;
+    private DriverConocimientosEsp control;
+    
+    
+    
+    
+    
+    public VentanaConociemientosEspec(){
+        
+        super ("Conociminetos Específicos");
+        InitComponents();
+        AcommodateCOmponents();
+        
+        pack();
+        
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(1100, 400);
+        setResizable(true);
+        setVisible(true);
+        
+    }
+
+    private void InitComponents() {
+        
+        //otros
+        
+        eventmanager = new EventManager(this);
+        control = new DriverConocimientosEsp();
+        
+        //paneles
+        JPtitulo = new JPanel();
+        JPconocimientos = new JPanel();
+        JPbotones = new JPanel();
+        
+        //etiquetas
+        JLtitulo = new JLabel("Conocimientos Específicos");
+        JLsubtitulo = new JLabel("Conocimiento");
+        JLsubtitulonivel = new JLabel("Nivel");
+        JLexp_des_cont_edu_dig = new JLabel("Experiencia en desarrollo de contenidos educativos digitales");
+        JLexp_des_elemnts_eval_comp = new JLabel("Experiencia en desarrollo de elementos de evaluación de competencias");
+        JLexp_des_lib_dig = new JLabel("Experiencia en desarrollo de libros de texto digital");
+        JLexp_gest_proy_TIC = new JLabel("Experiencia en gestión de proyectos educativos TIC");
+        JLexp_proc_elearning = new JLabel("Experiencia en procesos de e-­‐learning");
+        JLherram_Ofimatic = new JLabel("Conocimiento y manejo de herramientas ofimáticas");
+        JLherram_Web2 = new JLabel("Conocimiento y manejo de herramientas Web 2");
+        JLherram_edic_multi = new JLabel("Conocimiento herramientas de edición multimedia");
+        
+        //combobox
+        niveles = new JComboBox[8];
+        
+        for (int i = 0; i < niveles.length; i++) {
+            niveles[i] = new JComboBox(items);
+            
+        }
+        
+        //botones
+        
+        JBback = new JButton("Atras");
+        JBcanelar = new JButton("Cancelar");
+        JBnext = new JButton("Siquiente");
+        
+        
+        
+    }
+
+    private void AcommodateCOmponents() {
+        
+        container = this.getContentPane();
+        container.setLayout(new BorderLayout());
+        
+        
+        //titulo
+        
+        JPtitulo.setLayout(new BorderLayout());
+        JLtitulo.setHorizontalAlignment(SwingConstants.CENTER);
+        JPtitulo.add(JLtitulo, BorderLayout.CENTER);
+        
+        container.add(JPtitulo, BorderLayout.NORTH);
+        
+        //conocimientos
+        
+        JPconocimientos.setLayout(new GridLayout(9, 2, 5, 5));
+        
+        JLsubtitulo.setHorizontalAlignment(SwingConstants.CENTER);
+        JPconocimientos.add(JLsubtitulo);
+        
+//        JLsubtitulonivel.setHorizontalAlignment(SwingConstants.EAST);
+        JPconocimientos.add(JLsubtitulonivel);
+        
+        JPconocimientos.add(JLherram_Ofimatic);
+        JPconocimientos.add(niveles[0]);
+        
+        JPconocimientos.add(JLherram_Web2);
+        JPconocimientos.add(niveles[1]);
+        
+        JPconocimientos.add(JLherram_edic_multi);
+        JPconocimientos.add(niveles[2]);
+        
+        JPconocimientos.add(JLexp_des_cont_edu_dig);
+        JPconocimientos.add(niveles[3]);
+        
+        JPconocimientos.add(JLexp_des_lib_dig);
+        JPconocimientos.add(niveles[4]);
+        
+        JPconocimientos.add(JLexp_proc_elearning);
+        JPconocimientos.add(niveles[5]);
+        
+        JPconocimientos.add(JLexp_gest_proy_TIC);
+        JPconocimientos.add(niveles[6]);
+        
+        JPconocimientos.add(JLexp_des_elemnts_eval_comp);
+        JPconocimientos.add(niveles[7]);
+        
+        container.add(JPconocimientos, BorderLayout.CENTER);
+        
+        //botnoes
+        
+        JPbotones.setLayout(new GridLayout(1, 3, 8, 8));
+        
+        JPbotones.add(JBcanelar);
+        JPbotones.add(JBback);
+        JPbotones.add(JBnext);
+        
+        container.add(JPbotones, BorderLayout.SOUTH);
+        
+    }
+    
+    public class EventManager implements ActionListener {
+
+        private Component parentComponent;
+        
+        public EventManager (Component parentComponent){
+        
+            this.parentComponent = parentComponent;
+        
+        }
+        @Override
+        public void actionPerformed(ActionEvent e) {
+           
+            
+            if (e.getSource() == JBback){
+                
+                parentComponent.setVisible(false);
+                
+            
+            }
+            
+            if (e.getSource() == JBcanelar){
+            }
+            
+            if (e.getSource() == JBnext){
+                
+                String [] resultados = new String[8];
+                
+                for (int i = 0; i < resultados.length; i++) {
+                    
+                    resultados[i] = (String) niveles[i].getSelectedItem() ;
+                    
+                }
+                
+                control.guaradarConociemientoEspecifico(null, resultados[0], resultados[1], resultados[2], resultados[3], resultados[4], resultados[5], resultados[6], resultados[7], control.calcularPuntuacionTotal(resultados));
+                
+            }
+            
+            
+        }
+    
+    
+        
+    }
+    
+    public static void main(String args[]){
+
+    VentanaConociemientosEspec ven =  new VentanaConociemientosEspec();
+
+}  
+   
+}
---
+++ src/controlador/DriverConocimientosEsp.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+import AccesoDatos.*;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import logica.conocimientosEspecificos;
+
+/**
+ *
+ * @author jhonkrave
+ */
+public class DriverConocimientosEsp {
+    
+    DaoconociminetoEspecific Daoce;
+
+    public DriverConocimientosEsp() {
+    
+        Daoce = new DaoconociminetoEspecific();
+    
+    }
+    
+    
+    public void guaradarConociemientoEspecifico (String id, String JLherram_Ofimatic, String JLherram_Web2, 
+                                                    String JLherram_edic_multi, String JLexp_des_cont_edu_dig, 
+                                                    String JLexp_des_lib_dig, String JLexp_proc_elearning, String JLexp_gest_proy_TIC, 
+                                                    String JLexp_des_elemnts_eval_comp, double puntuacion){
+    
+        conocimientosEspecificos ce = new conocimientosEspecificos(id, JLherram_Ofimatic, JLherram_Web2, JLherram_edic_multi, JLexp_des_cont_edu_dig, JLexp_des_lib_dig, JLexp_proc_elearning, JLexp_gest_proy_TIC, JLexp_des_elemnts_eval_comp, puntuacion);
+        try {
+            Daoce.InsertConocimientoEspficic(ce);
+            System.out.println("se alamacenado con exito los cono_espcific");
+        
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Se ha guardado el aspirante con exito", "Registro exitoso", JOptionPane.INFORMATION_MESSAGE);
+            System.out.println("eerro al guardar conoc_espefici");
+            
+        }
+    
+    }
+    
+   
+    public double calcularPuntuacionTotal(String [] results){
+    
+        double resultado=0;
+        
+        for (int i = 0; i < results.length; i++) {
+           
+            resultado+= getPuntuacionNivel(results[i]);
+            
+        }
+    
+        return resultado;
+    }
+    
+    public double getPuntuacionNivel (String nivel){
+    
+        switch (nivel){
+        
+            case "Regular" : return 0.7;
+            case "Bueno"   : return  1.3;
+            case "Muy Bueno" : return 2.5;    
+            default: return 0;
+        }
+        
+       
+    
+    }
+}
