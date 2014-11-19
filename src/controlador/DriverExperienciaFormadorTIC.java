8ab92b0f613c34553a9d0b8aa738484c311e3118
gesveen <george.romero@correounivalle.edu.co>
2014-11-19 02:27:04 -0500

    actualizacion de el driver Exp Formador Tic por el puntaje a implementar

--- src/controlador/DriverExperienciaFormadorTIC.java
+++ src/controlador/DriverExperienciaFormadorTIC.java
@@ -22,10 +22,10 @@ public class DriverExperienciaFormadorTIC {
     }
     
     
-    public void guardarExperiencia(String id, String ExpFormerProf, String ExpFormerEst, String ExpFormertoFormers, String soporte_est, String soporte_pro, String soporte_for, int puntaje_est, int puntaje_pro, int puntaje_for){
+    public void guardarExperiencia(String id, String ExpFormerProf, String ExpFormerEst, String ExpFormertoFormers, String soporte_est, String soporte_pro, String soporte_for, int puntaje_est, int puntaje_pro, int puntaje_for, int puntaje_to, String cod_convo){
     
        
-        ExpFormadorTIC expFormerTIC = new ExpFormadorTIC(id, ExpFormerProf, ExpFormerEst, ExpFormertoFormers, soporte_est, soporte_pro, soporte_for, puntaje_est, puntaje_pro, puntaje_for);
+        ExpFormadorTIC expFormerTIC = new ExpFormadorTIC(id, ExpFormerProf, ExpFormerEst, ExpFormertoFormers, soporte_est, soporte_pro, soporte_for, puntaje_est, puntaje_pro, puntaje_for, puntaje_to, cod_convo);
         
         DaoExpFormerTIc.insertarExperiencia(expFormerTIC);
         
@@ -33,10 +33,10 @@ public class DriverExperienciaFormadorTIC {
     }
     
     
-    public String consultarSoporte(String identificacion, String titutlo, String sopor, String tipo_titu){
+    public String consultarSoporte(String identificacion, String titutlo, String sopor, String tipo_titu, String cod_convo){
         String link_soporte="";
         try{
-            link_soporte = DaoExpFormerTIc.consultarSoporte(identificacion, titutlo, sopor, tipo_titu);
+            link_soporte = DaoExpFormerTIc.consultarSoporte(identificacion, titutlo, sopor, tipo_titu, cod_convo);
             
             
         }catch(SQLException ex){
@@ -46,6 +46,12 @@ public class DriverExperienciaFormadorTIC {
     }
     
     
-    
+    public void asignarPuntajeAspirante(String identificacion, String convocatoria, int puntaje){
+        try{
+            DaoExpFormerTIc.asignarPuntajeAspirante(identificacion, convocatoria, puntaje);
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Error al insertar el puntaje del aspirante en la convocatoria. Por favor intente nuevamente");
+        }
+    }
     
 }
