207c2368eb07f4082f477476f3f70f44d2ccfbc6
KellysSanta <kellys.santa@correounivalle.edu.co>
2014-12-09 16:40:08 -0500

    logica

--- src/logica/formacionTic.java
+++ src/logica/formacionTic.java
@@ -12,6 +12,11 @@ public class formacionTic {
     private int puntaje;
     private String convocatoria;
     
+        
+    public formacionTic(){
+        
+    }
+    
     public formacionTic(String identificacion, String titulo, int consecutivo, String soporte, int puntaje, String convocatoria){
         
         this.identificacion = identificacion;
